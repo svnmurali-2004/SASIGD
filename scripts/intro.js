15d37223b9307bb05e6c5405ef8b73c1620f0b23
svnmurali-2004 <123376873+svnmurali-2004@users.noreply.github.com>
2025-02-23 22:10:45 +0530

    slide show added

---
+++ scripts/intro.js
@@ -0,0 +1,18 @@
+
+  // Array of background images
+  const images = [
+    "../img/intro-bg1.jpg",
+    "../img/intro-bg2.jpg",
+    "../img/intro-bg3.jpg"
+  ];
+  
+  let currentIndex = 0;
+  const introSection = document.getElementById("intro");
+  console.log("introSection", introSection);
+  function changeBackground() {
+    currentIndex = (currentIndex + 1) % images.length;
+    introSection.style.backgroundImage = `url('${images[currentIndex]}')`;
+  }
+
+  // Change background every 5 seconds
+  setInterval(changeBackground, 5000);
